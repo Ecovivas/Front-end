4a0634486cc290be746ea36b5843e92d41dfb8f9
Nesrux <joaomarcosdevs@gmail.com>
2026-04-06 21:10:40 -0300

    feat: adiciona novas funcionalidades

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("loginForm");
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  }
+});
